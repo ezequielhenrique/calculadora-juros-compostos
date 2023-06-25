ee0377c896c9ec6a3130048bc18358334433c039
ezequielhenrique <ezequielhenrique806@gmail.com>
2023-06-25 16:15:55 -0300

    dinamic chart created

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,53 @@ const inputFee = document.querySelector('#fee');
 const inputTime = document.querySelector('#time');
 const total = document.querySelector('.total');
 const buttonCalculate = document.querySelector('.calculate')
+const ctx = document.getElementById('myChart');
+
+
+const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        datasets: [{
+          data: [],
+        }],
+        labels: []
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+        y: {
+            beginAtZero: false
+        }
+      }
+    }
+});
+
+
+function updateChartData(chart, label, data) {
+    chart.data.labels = label;
+    chart.data.datasets.pop();
+
+    chart.data.datasets.push({
+        label: 'Valor',
+        data: data,
+    })
+
+    chart.update();
+}
 
 
 function calculaJuros(value, fee, time) {
-    return value*Math.pow((1 + fee), time);
+    let listValues = new Array(time);
+
+    for (let i=0; i<time; i++) {
+        listValues[i] = value.toFixed(2);
+        value += value*fee;
+    }
+
+    listValues.push(value.toFixed(2));
+
+    return [value, listValues];
 }
 
 
@@ -18,5 +61,7 @@ buttonCalculate.addEventListener('click', function () {
 
     let amount = calculaJuros(value, fee, time);
 
-    total.innerHTML = `R$ ${amount.toFixed(2)}`.replace('.', ',');
+    total.innerHTML = `R$ ${amount[0].toFixed(2)}`.replace('.', ',');
+
+    updateChartData(chart, new Array(amount[1].length).fill(''), amount[1]);
 });
